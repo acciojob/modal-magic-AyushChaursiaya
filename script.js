8bfa301ec9d45fe6d90545d711801114f593255c
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-05-22 19:24:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const openModalBtn = document.getElementById('openModalBtn');
+const modal = document.getElementById('modal');
+const closeModalBtn = document.getElementById('closeModalBtn');
+
+openModalBtn.addEventListener('click', function() {
+    modal.style.display = 'flex';
+})
+
+closeModalBtn.addEventListener('click', function() {
+    modal.style.display = 'none';
+})
